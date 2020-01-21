3b5bce04f66e11838d0ced36233f5e57fb102ff3
Florian <florian.berrot92@gmail.com>
2020-01-21 15:28:55 +0100

    add ins

--- Ywydev/src/Migrations/Version20200121132352.php
+++ Ywydev/src/Migrations/Version20200121132352.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20200121094242 extends AbstractMigration
+final class Version20200121132352 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -22,7 +22,8 @@ final class Version20200121094242 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('CREATE TABLE user_details (id INT AUTO_INCREMENT NOT NULL, roles TINYINT(1) NOT NULL, dob DATETIME NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE user ADD roles VARCHAR NOT NULL, ADD dob DATETIME NULL DEFAULT \'1970-01-02\' ');
+        $this->addSql('DROP TABLE user_details');
     }
 
     public function down(Schema $schema) : void
@@ -30,6 +31,6 @@ final class Version20200121094242 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('DROP TABLE user_details');
+        $this->addSql('ALTER TABLE user DROP roles, DROP dob');
     }
 }
