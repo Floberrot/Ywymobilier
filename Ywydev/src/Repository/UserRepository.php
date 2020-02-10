d2c42fa50856e9d5a494edf44c877258abebe50c
theejkb <steven.copy@ynov.com>
2020-02-10 17:42:51 +0100

    Relation 1-N for offers

--- Ywydev/src/Repository/UserRepository.php
+++ Ywydev/src/Repository/UserRepository.php
@@ -59,15 +59,15 @@ class UserRepository extends ServiceEntityRepository implements PasswordUpgrader
     }
     */
 
-    /*
-    public function findOneBySomeField($value): ?User
+
+    public function findOneById($userId): ?User
     {
         return $this->createQueryBuilder('u')
-            ->andWhere('u.exampleField = :val')
-            ->setParameter('val', $value)
+            ->andWhere('u.id = :id')
+            ->setParameter('id', $userId)
             ->getQuery()
             ->getOneOrNullResult()
         ;
     }
-    */
+
 }
