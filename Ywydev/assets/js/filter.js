4cf5d31932b56d871f03404253ef0646a5643616
theejkb <steven.copy@ynov.com>
2020-02-11 15:43:06 +0100

    add changes details

--- Ywydev/assets/js/filter.js
+++ Ywydev/assets/js/filter.js
@@ -21,17 +21,6 @@ new Vue({
             const { data } = await axios.get('/offres/get');
             this.properties = data;
         },
-        // coucou() {
-        //     alert('coucou !');
-        // },
-        // setAgeSorting() {
-            // const nextSteps = {
-            //     '0': 1,
-            //     '1': -1,
-            //     '-1': 0,
-            // };
-            // this.ageSorting = nextSteps[this.ageSorting.toString()];
-    //     },
     },
     mounted() {
         this.fetchUsers();
