1663058bb8ae812e94794c83e6e988e429032dc1
Florian <florian.berrot92@gmail.com>
2020-03-17 08:50:50 +0100

    settings....

--- src/Migrations/Version20200317074938.php
+++ src/Migrations/Version20200317074938.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20200310102558 extends AbstractMigration
+final class Version20200317074938 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -22,7 +22,7 @@ final class Version20200310102558 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE property ADD image_name VARCHAR(255) NOT NULL, ADD updated_at DATETIME NOT NULL');
+        $this->addSql('ALTER TABLE property DROP image_name, DROP updated_at');
     }
 
     public function down(Schema $schema) : void
@@ -30,6 +30,6 @@ final class Version20200310102558 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE property DROP image_name, DROP updated_at');
+        $this->addSql('ALTER TABLE property ADD image_name VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, ADD updated_at DATETIME DEFAULT NULL');
     }
 }
